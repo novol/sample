e0785309175206140abe28c61bd87c5b77a1fa1c
i.novol <i.novol@gmail.com>
2016-02-19 07:15:36 +0900

    Pan,Pinch,mono対応

---
+++ t_BlackAndWhite/t_BlackAndWhite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  t_BlackAndWhite
+//
+//  Created by i.novol on 2016/02/11.
+//  Copyright © 2016年 白黒変換. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ t_BlackAndWhite/t_BlackAndWhiteTests/t_BlackAndWhiteTests.swift
@@ -0,0 +1,36 @@
+//
+//  t_BlackAndWhiteTests.swift
+//  t_BlackAndWhiteTests
+//
+//  Created by i.novol on 2016/02/11.
+//  Copyright © 2016年 白黒変換. All rights reserved.
+//
+
+import XCTest
+@testable import t_BlackAndWhite
+
+class t_BlackAndWhiteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
